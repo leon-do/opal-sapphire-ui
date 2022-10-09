33932ed24786fd0673f50997a9983071790a243b
leon-do <leondo123@gmail.com>
2022-10-09 11:39:50 -0400

    ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Opal from "../opal";
 import downloadPreimage from "../opal/downloadPreimage";
 
 export default function Home() {
+  const [amountInput, setAmountInput] = useState("100000000000000000");
+  const [preimageInput, setPreimageInput] = useState("");
+  const [toInput, setToInput] = useState("");
   const opal = new Opal();
 
   const handleDeposit = async () => {
@@ -11,10 +15,22 @@ export default function Home() {
     }
     const preimage = opal.preimage();
     const hash = opal.hash(preimage);
-    const wei = "1000000000000000000";
-    downloadPreimage(`opal-${new Date()}.txt`, preimage);
+    // downloadPreimage(`opal-${new Date()}.txt`, preimage);
     try {
-      const txHash = await opal.deposit(hash, wei);
+      const txHash = await opal.deposit(hash, amountInput);
+      console.log(txHash);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleWithdraw = async () => {
+    if (!(await opal.isConnected())) {
+      const connected = await opal.connectWallet();
+      if (!connected) return console.error("Failed to connect to wallet");
+    }
+    try {
+      const txHash = await opal.withdraw(preimageInput, toInput);
       console.log(txHash);
     } catch (err) {
       console.error(err);
@@ -23,7 +39,12 @@ export default function Home() {
 
   return (
     <div>
+      <input value={amountInput} onChange={(e) => setAmountInput(e.target.value)} />
       <button onClick={() => handleDeposit()}>Deposit</button>
+      <hr />
+      <input value={preimageInput} onChange={(e) => setPreimageInput(e.target.value)} placeholder="123" />
+      <input value={toInput} onChange={(e) => setToInput(e.target.value)} placeholder="0xalice" />
+      <button onClick={() => handleWithdraw()}>Withdraw</button>
     </div>
   );
 }
